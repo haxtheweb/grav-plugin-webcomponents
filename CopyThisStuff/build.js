669089ab7b28016870a4fde9460971aceb9e0441
btopro <btopro@outlook.com>
2019-08-06 09:35:40 -0400

    build

--- CopyThisStuff/build.js
+++ CopyThisStuff/build.js
@@ -6,3 +6,4 @@ import "@lrnwebcomponents/hax-body/hax-body.js";
 import "@lrnwebcomponents/hax-body-behaviors/hax-body-behaviors.js";
 // common in CMSs
 import "@lrnwebcomponents/cms-hax/cms-hax.js";
+import "@lrnwebcomponents/wysiwyg-hax/wysiwyg-hax.js";
