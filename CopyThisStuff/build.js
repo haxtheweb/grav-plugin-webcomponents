f1ac7b5ca7d393a527a669500132eee41e9ae413
btopro <btopro@outlook.com>
2019-08-06 09:34:09 -0400

    package update

--- CopyThisStuff/build.js
+++ CopyThisStuff/build.js
@@ -4,8 +4,5 @@ import "@lrnwebcomponents/baseline-build-hax/baseline-build-hax.js";
 import "@lrnwebcomponents/h-a-x/h-a-x.js";
 import "@lrnwebcomponents/hax-body/hax-body.js";
 import "@lrnwebcomponents/hax-body-behaviors/hax-body-behaviors.js";
-
 // common in CMSs
 import "@lrnwebcomponents/cms-hax/cms-hax.js";
-// used in Grav, wysiwyg textarea drop in
-import "@lrnwebcomponents/wysiwyg-hax/wysiwyg-hax.js";
